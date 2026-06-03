e39327421d5778f704fda921efabd5a0915b8d24
sanpawat <koyong555@hotmail.com>
2026-06-03 12:16:51 +0700

    Add Swift Playgrounds icon and adjust Learn component position
    Introduced a new Swift Playgrounds icon image to the public directory. Adjusted the position of the Learn component in the App.tsx file to ensure it is displayed correctly within the main content layout.

--- src/App.tsx
+++ src/App.tsx
@@ -21,8 +21,8 @@ export default function App() {
         <About />
         <Format />
         <Eligibility />
-        <Learn />
         <Timeline />
+        <Learn />
         <Organizers />
       </main>
       <Footer />
